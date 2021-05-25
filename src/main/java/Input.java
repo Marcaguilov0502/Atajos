b58b8703820e9308f83eeb68b5ffab57067cf36d
Marcaguilov0502 <74609724+Marcaguilov0502@users.noreply.github.com>
2021-05-25 19:43:39 +0200

    Update Input.java

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public abstract class Input {
@@ -5,44 +6,110 @@ public abstract class Input {
     public static Scanner input = new Scanner(System.in);
 
     public static byte getByte() {
-        byte sysIn = input.nextByte();
-        String aux = input.nextLine();
-        return sysIn;
+        try {
+            byte sysIn = input.nextByte();
+            String aux = input.nextLine();
+            return sysIn;
+        } catch (InputMismatchException ime) {
+            String aux = input.nextLine();
+            System.out.println("Valor introducido incorrecto");
+            System.out.println("Escribe un numero valido: ");
+            return getByte();
+        }
     }
 
     public static short getShort() {
-        short sysIn = input.nextShort();
-        String aux = input.nextLine();
-        return sysIn;
+        try {
+            short sysIn = input.nextShort();
+            String aux = input.nextLine();
+            return sysIn;
+        } catch (InputMismatchException ime) {
+            String aux = input.nextLine();
+            System.out.println("Valor introducido incorrecto");
+            System.out.println("Escribe un numero valido: ");
+            return getShort();
+        }
     }
 
     public static int getInt() {
-        int sysIn = input.nextInt();
-        String aux = input.nextLine();
-        return sysIn;
+        try {
+            int sysIn = input.nextInt();
+            String aux = input.nextLine();
+            return sysIn;
+        } catch (InputMismatchException ime) {
+            String aux = input.nextLine();
+            System.out.println("Valor introducido incorrecto");
+            System.out.println("Escribe un numero valido: ");
+            return getInt();
+        }
     }
 
     public static long getLong() {
-        long sysIn = input.nextLong();
-        String aux = input.nextLine();
-        return sysIn;
+        try {
+            long sysIn = input.nextLong();
+            String aux = input.nextLine();
+            return sysIn;
+        } catch (InputMismatchException ime) {
+            String aux = input.nextLine();
+            System.out.println("Valor introducido incorrecto");
+            System.out.println("Escribe un numero valido: ");
+            return getLong();
+        }
     }
 
     public static float getFloat() {
-        float sysIn = input.nextFloat();
-        String aux = input.nextLine();
-        return sysIn;
+        try {
+            String aux = input.nextLine();
+            aux = aux.replace(",",".");
+            float f = Float.parseFloat(aux);
+            if (f > Float.MAX_VALUE) {throw new NumberFormatException();}
+            return f;
+        } catch (NumberFormatException ime) {
+            System.out.println("Valor introducido incorrecto");
+            System.out.println("Escribe un numero valido: ");
+            return getFloat();
+        }
     }
 
     public static double getDouble() {
-        Double sysIn = input.nextDouble();
-        String aux = input.nextLine();
-        return sysIn;
+        try {
+            String aux = input.nextLine();
+            aux = aux.replace(",",".");
+            double d = Double.parseDouble(aux);
+            if (d > Double.MAX_VALUE) {throw new NumberFormatException();}
+            return d;
+        } catch (NumberFormatException ime) {
+            System.out.println("Valor introducido incorrecto");
+            System.out.println("Escribe un numero valido: ");
+            return getDouble();
+        }
     }
 
     public static String getString() {
-        String sysIn = input.nextLine();
-        return sysIn;
+        try {
+            String sysIn = input.nextLine();
+            return sysIn;
+        } catch (InputMismatchException ime) {
+            System.out.println("Valor introducido incorrecto");
+            System.out.println("Escribe un texto valido: ");
+            return getString();
+        }
+    }
+
+    public static char getChar() {
+        try {
+            String sysIn = input.nextLine();
+            if (sysIn.length() != 1) {
+                throw new  InputMismatchException();
+            }
+            else {
+                return sysIn.charAt(0);
+            }
+        } catch (InputMismatchException ime) {
+            System.out.println("Valor introducido incorrecto");
+            System.out.println("Escribe un caracter valido: ");
+            return getChar();
+        }
     }
 
 }
